c82cf538853601da037356a91b3a759465c104f7
Aryeh Silver <aryehsilver@hotmail.com>
2021-08-22 01:14:30 +0100

    Save locations in user's roaming folder to enable write access

--- GrabWindows10LockScreenPictures/Program.cs
+++ GrabWindows10LockScreenPictures/Program.cs
@@ -6,16 +6,28 @@ using System.Linq;
 
 namespace GrabWindows10LockScreenPictures {
   class Program {
+    static readonly string App_Folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Grab Windows 10 Lock Screen Pictures";
     static void Main(string[] args) {
-      string[] users = File.ReadAllLines(Path.GetFullPath("Data/locations.txt"));
-      string saved = @$"C:\Users\{users.FirstOrDefault()}\Pictures\Spotlight Images";
-
       try {
-        foreach (string name in users) {
-          GetPics(@$"C:\Users\{name}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets", saved);
+        string userName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Replace(@"C:\Users\", "");
+
+        if (!Directory.Exists(App_Folder)) {
+          Directory.CreateDirectory(App_Folder);
+          File.WriteAllText(Path.Combine(App_Folder, "locations.txt"), userName);
         }
 
-        PopTheToast.PopIt("All images were successfully saved.");
+        if (File.Exists(Path.Combine(App_Folder, "locations.txt"))) {
+          string[] users = File.ReadAllLines(Path.Combine(App_Folder, "locations.txt"));
+          string saved = @$"C:\Users\{users.FirstOrDefault()}\Pictures\Spotlight Images";
+
+          foreach (string name in users) {
+            GetPics(@$"C:\Users\{name}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets", saved);
+          }
+
+          PopTheToast.PopIt("All images were successfully saved.");
+        } else {
+          PopTheToast.PopIt("Error: locations.txt file was not found.");
+        }
       } catch (Exception ex) {
         PopTheToast.PopIt("An error occurred: " + ex.Message);
       }
