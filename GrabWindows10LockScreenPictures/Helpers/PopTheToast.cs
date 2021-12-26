8751ca08be6d320d98b8e3cc130c6f076e03d9ca
Aryeh Silver <aryehsilver@hotmail.com>
2021-12-26 15:21:26 +0000

    Upgrade to .Net 6

--- GrabWindows10LockScreenPictures/Helpers/PopTheToast.cs
+++ GrabWindows10LockScreenPictures/Helpers/PopTheToast.cs
@@ -2,15 +2,15 @@
 using System;
 using System.IO;
 
-namespace GrabWindows10LockScreenPictures.Helpers {
-  public class PopTheToast {
-    public static void PopIt(string notifText) =>
-      new ToastContentBuilder()
-          .AddText("Grab Windows 10 Lock Screen Pictures")
-          .AddText(!string.IsNullOrWhiteSpace(notifText) ? notifText : "Script has run with an unknown result.")
-          .AddHeroImage(new Uri("file:///" + Path.GetFullPath("Data/buffelo.png")))
-          .AddAppLogoOverride(new Uri("file:///" + Path.GetFullPath("Data/lion.png")), ToastGenericAppLogoCrop.Circle)
-          .AddAttributionText("Via GWLSP")
-          .Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(10));
-  }
+namespace GrabWindows10LockScreenPictures.Helpers;
+
+public class PopTheToast {
+  public static void PopIt(string notifText) =>
+    new ToastContentBuilder()
+        .AddText("Grab Windows 10 Lock Screen Pictures")
+        .AddText(!string.IsNullOrWhiteSpace(notifText) ? notifText : "Script has run with an unknown result.")
+        .AddHeroImage(new Uri("file:///" + Path.GetFullPath("Data/buffelo.png")))
+        .AddAppLogoOverride(new Uri("file:///" + Path.GetFullPath("Data/lion.png")), ToastGenericAppLogoCrop.Circle)
+        .AddAttributionText("Via GWLSP")
+        .Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(10));
 }
--- GrabWindows10LockScreenPictures/Program.cs
+++ GrabWindows10LockScreenPictures/Program.cs
@@ -4,43 +4,43 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 
-namespace GrabWindows10LockScreenPictures {
-  class Program {
-    static readonly string App_Folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Grab Windows 10 Lock Screen Pictures";
+namespace GrabWindows10LockScreenPictures;
 
-    static void Main(string[] args) {
-      try {
-        string userName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Replace(@"C:\Users\", "");
+class Program {
+  static readonly string App_Folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Grab Windows 10 Lock Screen Pictures";
 
-        if (!Directory.Exists(App_Folder)) {
-          Directory.CreateDirectory(App_Folder);
-          File.WriteAllText(Path.Combine(App_Folder, "locations.txt"), userName);
-        }
+  static void Main(string[] args) {
+    try {
+      string userName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Replace(@"C:\Users\", "");
 
-        if (File.Exists(Path.Combine(App_Folder, "locations.txt"))) {
-          string[] users = File.ReadAllLines(Path.Combine(App_Folder, "locations.txt"));
-          string saved = @$"C:\Users\{users.FirstOrDefault()}\Pictures\Spotlight Images";
-
-          foreach (string name in users) {
-            GetPics(@$"C:\Users\{name}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets", saved);
-          }
-
-          PopTheToast.PopIt("All images were successfully saved.");
-        } else {
-          PopTheToast.PopIt("Error: locations.txt file was not found.");
-        }
-      } catch (Exception ex) {
-        PopTheToast.PopIt("An error occurred: " + ex.Message);
+      if (!Directory.Exists(App_Folder)) {
+        Directory.CreateDirectory(App_Folder);
+        File.WriteAllText(Path.Combine(App_Folder, "locations.txt"), userName);
       }
-    }
 
-    static void GetPics(string assets, string saved) {
-      foreach (string file in Directory.GetFiles(assets).Where(f => new FileInfo(f).Length > 200 * 1024)) {
-        Image img = Image.FromFile(file);
-        if (img.Width > img.Height) {
-          string newFile = saved + file.Replace(assets, "") + ".jpg";
-          File.Copy(file, newFile, true);
+      if (File.Exists(Path.Combine(App_Folder, "locations.txt"))) {
+        string[] users = File.ReadAllLines(Path.Combine(App_Folder, "locations.txt"));
+        string saved = @$"C:\Users\{users.FirstOrDefault()}\Pictures\Spotlight Images";
+
+        foreach (string name in users) {
+          GetPics(@$"C:\Users\{name}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets", saved);
         }
+
+        PopTheToast.PopIt("All images were successfully saved.");
+      } else {
+        PopTheToast.PopIt("Error: locations.txt file was not found.");
+      }
+    } catch (Exception ex) {
+      PopTheToast.PopIt("An error occurred: " + ex.Message);
+    }
+  }
+
+  static void GetPics(string assets, string saved) {
+    foreach (string file in Directory.GetFiles(assets).Where(f => new FileInfo(f).Length > 200 * 1024)) {
+      Image img = Image.FromFile(file);
+      if (img.Width > img.Height) {
+        string newFile = saved + file.Replace(assets, "") + ".jpg";
+        File.Copy(file, newFile, true);
       }
     }
   }
